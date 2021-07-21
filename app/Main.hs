9c2b62573bddf0f29aaca2a3a817af23d0df3c9e
Chaitanya Kukde <ckukde@gmail.com>
2021-07-21 12:01:52 +0100

    KDTree render with weird alignment

--- app/Main.hs
+++ app/Main.hs
@@ -6,36 +6,20 @@ import Diagrams.Backend.SVG.CmdLine
 import Diagrams.TwoD.Shapes
 import Diagrams.TwoD.Text
 
---example :: Diagram B
---example = circle 1 # fc red # lw none ||| circle 1 # fc green # lw none
---example = square 1 # fc aqua
---lhs = rect 2 5 # fc green === square 2 # fc red
---rhs = square 2 # fc yellow === rect 2 5 # fc aqua
-
-
---w = 2
---h = 5
---lhs1 = text "f1" <> rect w h 
---lhs2 = text "f2" <> square w
---lhs  = lhs1 === lhs2 
---rhs1 = text "f3" <> square w
---rhs2 = text "f4" <> rect w h
---rhs  = rhs1 === rhs2
---example = center $ alignT lhs ||| alignT rhs
-
 data Cut = Horizontal | Vertical  deriving (Eq, Show, Read)
 
+--Binary Tree with Cut
 data Tree a = Leaf a | Node Cut (Tree a) (Tree a) deriving (Show, Read)
 type STree = Tree String 
 binTree :: STree
 binTree = Node Vertical (Node Horizontal (Leaf "f1") (Leaf "f2")) (Node Horizontal (Leaf "f3") (Leaf "f4"))
 
-treeToString :: Tree String -> String 
+treeToString :: STree -> String 
 treeToString (Leaf n) = "leaf - " ++ show n
 treeToString (Node cut lt rt) = "cut - " ++ show cut ++ " | " ++ treeToString lt ++ treeToString rt
 
-treeToDiag :: Tree String -> Diagram B
 side = 5
+treeToDiag :: STree -> Diagram B
 treeToDiag (Leaf a) = text a <> square side
 treeToDiag (Node cut lt rt) = do
     if cut == Horizontal
@@ -44,5 +28,25 @@ treeToDiag (Node cut lt rt) = do
         then treeToDiag lt ||| treeToDiag rt
     else text "Error" <> square side
 
+-- KD Tree
 
-main = mainWith $ treeToDiag binTree
+data KDTree morphism object = KLeaf morphism [object] [object] | KNode Cut (KDTree morphism object) (KDTree morphism object) deriving (Show, Read)
+type KDString = KDTree String String
+f1 = KLeaf "f1" ["x1"] ["x3", "x4"]
+f2 = KLeaf "f2" ["x2"] ["x5"]
+f3 = KLeaf "f3" ["x3"] ["x6"]
+f4 = KLeaf "f4" ["x4", "x5"] ["x7"]
+mytree :: KDString
+mytree = KNode Vertical (KNode Horizontal f1 f2) (KNode Horizontal f3 f4)
+
+kdtreeToDiag :: KDString -> Diagram B
+kdtreeToDiag (KLeaf t xs ys) = text t <> rect (side * fromIntegral(length xs)) (side * fromIntegral(length ys))
+kdtreeToDiag (KNode cut lt rt) = do
+    if cut == Horizontal
+        then kdtreeToDiag lt === kdtreeToDiag rt
+    else if cut == Vertical
+        then alignT (kdtreeToDiag lt) ||| alignT (kdtreeToDiag rt)
+    else text "Error" <> square side
+
+
+main = mainWith $ kdtreeToDiag mytree
