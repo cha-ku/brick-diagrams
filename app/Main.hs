4e0672333dc8cadf48d26738dfb4fc0f9edca19a
Chaitanya Kukde <ckukde@gmail.com>
2021-08-15 16:16:35 +0100

    Added kdtreeToString function

--- app/Main.hs
+++ app/Main.hs
@@ -20,6 +20,13 @@ kdtreeToDiag (KNode cut lt rt) =
     case cut of Horizontal sf -> kdtreeToDiag lt # scaleY (sf * side) === kdtreeToDiag rt # scaleY ((1-sf) * side)
                 Vertical      -> alignT (kdtreeToDiag lt # scaleX (0.5 * side))  ||| alignT (kdtreeToDiag rt # scaleX (0.5 * side))
 
+kdtreeToString :: KDString -> String
+kdtreeToString (KLeaf t xs ys) = show t ++ show xs ++ show ys ++ show "\n"
+kdtreeToString (KNode cut lt rt) =
+    -- get user to supply the overlap ratio
+    case cut of Horizontal _ -> kdtreeToString lt ++ " : Horizontally composed with " ++ kdtreeToString rt ++ show "\n"
+                Vertical     -> kdtreeToString lt ++ " : Vertically composed with " ++ kdtreeToString rt ++ show "\n"
+
 f1 = KLeaf "f1" ["x1"] ["x3", "x4"]
 f2 = KLeaf "f2" ["x2"] ["x5"]
 f3 = KLeaf "f3" ["x3"] ["x6"]
@@ -28,4 +35,5 @@ f4 = KLeaf "f4" ["x4", "x5"] ["x7"]
 mytree :: KDString
 mytree = KNode Vertical (KNode (Horizontal 0.66) f1 f2) (KNode (Horizontal 0.33) f3 f4)
 
-main = mainWith $ alignT $ kdtreeToDiag mytree
+main = do
+    putStrLn (kdtreeToString mytree)
